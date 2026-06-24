3eb7bfb7d5a2868ad0967b51c31e491a53eaf4fa
Andrea Rossi <a.rossi.andrea@gmail.com>
2026-06-24 14:01:10 +0200

    fix opaque background webxr viewer

--- src/scene/renderer.js
+++ src/scene/renderer.js
@@ -4,10 +4,12 @@ export function createRenderer({ alpha = true } = {}) {
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha,
+    premultipliedAlpha: true,
   });
 
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x000000, alpha ? 0 : 1);
   renderer.xr.enabled = true;
 
   return renderer;
